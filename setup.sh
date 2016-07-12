b47582a6d7de29fdb1a9eb3a2de4cf5ae5e5ecd4
Dave Glover <dglover@microsoft.com>
2016-07-12 16:45:56 +1000

    moved explorer hat install to the end with its prompted reboot

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ sudo pip install paho-mqtt -q
 sudo apt-get install xrdp -y
 sudo apt-get install iceweasel -y
 
-curl -sS get.pimoroni.com/explorerhat | bash
-
 chmod +x *.sh
 chmod +x *.py
 
 sudo cp /home/pi/drumkit/drumkit.service /etc/systemd/system/
 sudo systemctl enable drumkit.service
+
+curl -sS get.pimoroni.com/explorerhat | bash
