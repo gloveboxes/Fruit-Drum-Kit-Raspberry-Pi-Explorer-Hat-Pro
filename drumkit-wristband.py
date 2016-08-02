cfbf881ad922849b468e81927c3d562b374da312
Dave Glover <dglover@microsoft.com>
2016-08-02 23:02:34 +1000

    update with new sounds

--- drumkit-wristband.py
+++ drumkit-wristband.py
@@ -12,7 +12,6 @@ import signal
 import pygame
 import explorerhat
 import json
-import base64
 
 # https://pypi.python.org/pypi/paho-mqtt
 # http://www.eclipse.org/paho/clients/python/docs/
@@ -30,63 +29,37 @@ light = [{'MsgId':0,'LightId':[1,2],'Red':0,'Green':0,'Blue':255},
          {'MsgId':0,'LightId':[1,2],'Red':0,'Green':255,'Blue':0}
 ]
 
-dmxMsg = {'id':[1],'data':'data'}
-
 LEDS = [4, 17, 27, 5]
-drumMode = 0;
-explorerhat.light.blue.on()
-
 
 samples = [
-    #'sounds/drums/hat.wav',
-    #'sounds/drums/smash.wav',
-    #'sounds/drums/rim.wav',
-    #'sounds/drums/ting.wav',
-    'sounds/drums/hit.wav',
-    'sounds/drums/thud.wav',
-    'sounds/drums/clap.wav',
-    'sounds/drums/crash.wav',
-    'sounds/balmer/iltc_I.wav',
-    'sounds/balmer/iltc_love.wav',
-    'sounds/balmer/iltc_this.wav',
-    'sounds/balmer/iltc_company.wav',
-    'sounds/beep.wav',
+    'sounds/hat.wav',
+    'sounds/smash.wav',
+    'sounds/rim.wav',
+    'sounds/ting.wav',
+    'sounds/hit.wav',
+    'sounds/thud.wav',
+    'sounds/clap.wav',
+    'sounds/crash.wav',
 ]
-
 
 pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.mixer.init()
 pygame.mixer.set_num_channels(16)
 
 sounds = []
-for x in range(9):
+for x in range(8):
     sounds.append(pygame.mixer.Sound(samples[x]))
 
 
 def handle(ch, evt):
-    global drumMode
     if evt == 'press':
-        if ch == 1 or ch == 2:
-            print(str(ch))
-            explorerhat.light.blue.off()
-            explorerhat.light.yellow.off()        
-            drumMode = ch - 1            
-            sounds[8].play(loops=0)
-            explorerhat.light[drumMode].on()
-            return
-        if ch == 3 or ch == 4:
-            return
-        
         client.publish('dmx/data', json.dumps(light[ch-1]))
-        explorerhat.light.red.fade(0, 100, 0.1)
-        instrument = ((ch - 5) + (drumMode * 4))
-        #print("Instrument " + str(instrument))
-        
-        sounds[instrument].play(loops=0)
-        name = samples[instrument].replace('sounds/','').replace('.wav','')
+        explorerhat.light.blue.fade(0, 100, 0.1)
+        sounds[ch - 1].play(loops=0)
+        name = samples[ch - 1].replace('/home/pi/vivid/sounds/','').replace('.wav','')
         print("{}!".format(name.capitalize()))
     else:
-        explorerhat.light.red.off()
+        explorerhat.light.blue.off()
 
 def on_connect(client, userdata, rc):
     explorerhat.light.green.on()
--- start-drums.sh
+++ start-drums.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 2
 cd /home/pi/drumkit
-#./drumkit-rgb.py&
-./drumkit-wristband.py&
+./drumkit-rgb.py&
+#./drumkit-wristband.py&
